25a594102f2b087fd55890bac6e2fdacc5b751c8
Gaurav Chavda <gaurav.chavda@radixweb.com>
2024-06-01 18:41:27 +0530

    Fix build related error

--- src/components/TabTitle.vue
+++ src/components/TabTitle.vue
@@ -1,10 +1,10 @@
 <template>
     <ul id="myTab1" role="tablist" class="nav nav-tabs nav-pills with-arrow flex-column flex-sm-row text-center">
         <li class="nav-item flex-sm-fill">
-            <router-link to="/all-jobs" data-toggle="tab" role="tab"  class="nav-link text-uppercase font-weight-bold rounded-0 border" :class="{ 'active': this.$route.name === 'index' || this.$route.name === 'AllJobs' }" >Jobs</router-link>
+            <router-link to="/all-jobs" data-toggle="tab" role="tab"  class="nav-link text-uppercase font-weight-bold rounded-0 border" :class="{ 'active': $route.name === 'index' || $route.name === 'AllJobs' }" >Jobs</router-link>
         </li>
         <li class="nav-item flex-sm-fill">
-            <router-link to="/fav-jobs" data-toggle="tab" role="tab" class="nav-link text-uppercase font-weight-bold rounded-0 border " :class="{ 'active': this.$route.name === 'FevJob'}">Favorites</router-link>
+            <router-link to="/fav-jobs" data-toggle="tab" role="tab" class="nav-link text-uppercase font-weight-bold rounded-0 border " :class="{ 'active': $route.name === 'FevJob'}">Favorites</router-link>
         </li>
     </ul>
 </template>
